f4c86d341f626d310d5fe45d5c89aadc2973c2b5
Paul McCauley <paul_e_mccauley@yahoo.com>
2016-07-18 15:33:27 -0400

    Changes contain notes for running PowerShell commands and scripts from within Python

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,18 @@
 import os
 import subprocess
 
-subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe", "C:\\users\\pmccaul\\Desktop\\PowerShell_Scripts\\testScript.ps1"])
+# Call PowerShell and set execution policy to unrestricted
+#subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe", "set-executionpolicy unrestricted -force"])
 
-os.system('dir')
+# Run PowerShell command from Python script
+#subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe", "get-command"])
+
+# Run PowerShell script from Python script
+#subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe", "C:\\users\\pmccaul\\Desktop\\PowerShell_Scripts\\testScript.ps1"])
+
+# Get all computers in AD and assign to a variable $adComputers
+#subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe", "$adComputers = (get-adcomputer -filter * | sort-object | select Name)"])
+
+#os.system('dir')
+
 print("Hello World!")
