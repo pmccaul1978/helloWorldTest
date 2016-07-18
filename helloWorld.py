b381faf9cc4cb24cdeda9ff9971b7dc4e242c3c8
Paul McCauley <paul_e_mccauley@yahoo.com>
2016-07-18 14:52:55 -0400

    This is a test for committing changes.

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+import os
+import subprocess
+
+subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe", "C:\\users\\pmccaul\\Desktop\\PowerShell_Scripts\\testScript.ps1"])
+
+os.system('dir')
+print("Hello World!")
